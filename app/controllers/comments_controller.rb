e1ef23b4a0714c994ed72a371d2b741c51b96385
ssela98 <tirnavanalessandra@gmail.com>
2022-12-22 21:47:26 +0100

    add comments#index

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def index
+    @comments = @post.comments
+  end
+
+  private
+
+  def set_post
+    @post = Post.find_by(id: params[:post_id])
+  end
+end
