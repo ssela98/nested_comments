b840034472e39c2a81a2aeabba11f121afc00c90
ssela98 <tirnavanalessandra@gmail.com>
2022-12-22 21:48:38 +0100

    add comments#show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,20 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: :show
 
   def index
     @comments = @post.comments
   end
 
+  def show; end
+
   private
 
   def set_post
     @post = Post.find_by(id: params[:post_id])
+  end
+
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
   end
 end
